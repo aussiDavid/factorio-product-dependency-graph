f6761292d1d7b3fc00c61410084d84bdb5b885cb
David Milanese <milanese.david@gmail.com>
2021-04-04 19:48:04 +0930

    lint file

--- src/product_graph.lua
+++ src/product_graph.lua
@@ -7,14 +7,18 @@ local Map = require "map"
 
 local product_graph = {}
 
-function ingredients(product) 
-  local product = product or {}
+local function ingredient_name(ingredient)
+  return ingredient.name or ingredient[1]
+end
+
+local function ingredients_for(product)
+  product = product or {}
   local ingredients = {}
 
   if product.ingredients then
     ingredients = product.ingredients
   end
-  
+
   if product.normal and product.normal.ingredients then
     ingredients = product.normal.ingredients
   end
@@ -26,16 +30,14 @@ function ingredients(product)
   return Map(ingredients, ingredient_name)
 end
 
-function ingredient_name(value) return value.name or value[1] end
-
 function product_graph.find_dependency(product_name, recipes)
-  local recipes = recipes or RECIPES
+  recipes = recipes or RECIPES
   local product = recipes[product_name]
 
   return {
-    name=product_name,
-    items=Map(
-      ingredients(product),
+    name = product_name,
+    items = Map(
+      ingredients_for(product),
       function(item) return product_graph.find_dependency(item, recipes) end
     )
   }
