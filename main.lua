1239eb9f4e29e3196e207b9e079558e5de4bd1de
David Milanese <milanese.david@gmail.com>
2021-04-04 20:23:38 +0930

    Added main script to execute the program

---
+++ main.lua
@@ -0,0 +1,36 @@
+#!/usr/bin/env lua
+
+package.path = package.path .. ';./src/?.lua'
+package.path = package.path .. ';./lua_modules/share/lua/5.3/?.lua'
+
+local JSON = require "JSON"
+local product_graph = require "product_graph"
+
+local product_name = arg[1]
+
+if not product_name then
+  print("Error: Please provide a PRODUCT_NAME argument")
+  print([[
+Generates the dependencies for a PRODUCT_NAME in JSON format.
+
+Usage:
+  lua scratch.lua PRODUCT_NAME
+
+Example:
+  $ lua scratch.lua "steel-plate"
+
+  {
+    "items": [ {
+      "items": [ {
+        "items": [],
+        "name": "iron-ore"
+      } ],
+      "name": "iron-plate"
+    } ],
+    "name": "steel-plate"
+  }
+  ]])
+  os.exit(1)
+end
+
+print(JSON:encode_pretty(product_graph.find_dependency(product_name)))
